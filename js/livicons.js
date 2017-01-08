46770e42bdbae291e4bbb4d787655efda4f37126
pemudaID <randymandala@gmail.com>
2017-01-08 15:04:27 +0700

    1
    1

---
+++ js/livicons.js
@@ -0,0 +1,11 @@
+/*************************************************************************************
+ * @name: LivIcons - animated vector icons (minified)
+ * @version: 1.4
+ * @URL: http://livicons.com
+ * @copyright: (c) 2014 DeeThemes (http://codecanyon.net/user/DeeThemes)
+ * @licenses: http://codecanyon.net/licenses/regular
+              http://codecanyon.net/licenses/extended
+*************************************************************************************/
+
+// please note that these icons are disabled in live demo, but you will get a full version
+// of livicons with you purchased files.
